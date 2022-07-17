9417cb64beabc9a983b2ce3ae29378a075ffb8df
Aditi Datta <aditidatta373@gmail.com>
2022-07-17 20:57:32 +0600

    ss

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ async function run() {
 
         app.post('/banglaAttendance', async(req,res) => {
             const data = req.body;
-            const store = await attendanceCollection.insertOne(data);
+            const store = await attendanceCollection.insertMany(data);
             res.json(store) 
         });
 
