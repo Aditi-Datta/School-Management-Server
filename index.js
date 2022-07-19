050ddee20b39c507267cfc80980d0ae3f8481e27
Aditi Datta <aditidatta373@gmail.com>
2022-07-19 20:52:20 +0600

    add

--- index.js
+++ index.js
@@ -172,10 +172,10 @@ async function run() {
             const store = await attendanceCollection.insertMany(data);
             res.json(store);
         });
-        app.get('/banglaAttendance', async(req,res) => {
-            const store = await attendanceCollection.find({}).toArray();
-            res.json(store) 
-        });
+        // app.get('/banglaAttendance', async(req,res) => {
+        //     const store = await attendanceCollection.find({}).toArray();
+        //     res.json(store) 
+        // });
         app.post('/classSevenStudent', async (req, res) => {
             let {email}=req.body;
             const student = await classSevenStudentCollection.find({email}).toArray();      
