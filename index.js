3ba163fd91b56eadb72ba1e7f43ac4dc0b0f0591
Aditi Datta <aditidatta373@gmail.com>
2022-07-22 13:14:11 +0600

    add

--- index.js
+++ index.js
@@ -86,23 +86,35 @@ async function run() {
         })
 
         app.put('/users/admin', verifyToken, async (req, res) => {
-            const user = req.body;
-            const requester = req.decodedEmail;
-            if (requester) {
-                const requesterAccount = await usersCollection.findOne({ email: requester });
-
-                if (requesterAccount.role === 'admin') {
-                    const filter = { email: user.email };
-                    const updateDoc = { $set: { role: 'admin' } };
-                    const result = await usersCollection.updateOne(filter, updateDoc);
-                    res.json(result);
-                }
-            }
-            else {
-                res.status(403).json({ message: 'you do not have access to make admin' })
-            }
+           
+           const email = req.params.email;
+             const filter = { email: email };
+            
+             const updateDoc = { $set: { role: 'admin' } };
+             const result = await usersCollection.updateOne(filter, updateDoc);
+             res.json(result);
+               
 
         });
+
+        // app.put('/users/admin', verifyToken, async (req, res) => {
+        //     const user = req.body;
+        //     const requester = req.decodedEmail;
+        //     if (requester) {
+        //         const requesterAccount = await usersCollection.findOne({ email: requester });
+
+        //         if (requesterAccount.role === 'admin') {
+        //             const filter = { email: user.email };
+        //             const updateDoc = { $set: { role: 'admin' } };
+        //             const result = await usersCollection.updateOne(filter, updateDoc);
+        //             res.json(result);
+        //         }
+        //     }
+        //     else {
+        //         res.status(403).json({ message: 'you do not have access to make admin' })
+        //     }
+
+        // });
 
 
         app.get('/users/teacher/:email', async (req, res) => {
